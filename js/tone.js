7ce8106ba29a46b6847629dd3058b1d742bc3317
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-04-12 17:28:09 -0600

    feat: basic tone generation

---
+++ js/tone.js
@@ -0,0 +1,56 @@
+const A1 = 55;
+
+function note_to_frequency(note) {
+  var parsed = 0;
+  switch (note[0]) {
+    case "A":
+      parsed = 0;
+      break;
+    case "B":
+      parsed = 2;
+      break;
+    case "C":
+      parsed = 3;
+      break;
+    case "D":
+      parsed = 5;
+      break;
+    case "E":
+      parsed = 7;
+      break;
+    case "F":
+      parsed = 8;
+      break;
+    case "G":
+      parsed = 10;
+      break;
+  }
+  if (note[1] == "#") {
+    parsed += 1;
+    parsed += (note[2] - 1) * 12;
+  } else if (note[1] == "b") {
+    parsed -= 1;
+    parsed += (note[2] - 1) * 12;
+  } else {
+    parsed += (note[1] - 1) * 12;
+  }
+  return 55 * Math.pow(2, parsed / 12);
+}
+
+async function play() {
+  const note = document.getElementById("note").value;
+  const frequency = note_to_frequency(note);
+  await play_tone(frequency);
+}
+
+async function play_tone(frequency = 440) {
+  const context = new AudioContext();
+  const oscillator = context.createOscillator();
+  oscillator.frequency.value = frequency;
+  oscillator.connect(context.destination);
+  await context.resume();
+  oscillator.start();
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  oscillator.stop();
+  context.close();
+}
